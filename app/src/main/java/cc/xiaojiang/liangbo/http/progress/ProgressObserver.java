57a0134646641b2cebf95428445999fcf1f6e226
facexxyz <facexxyz@gmail.com>
2018-07-23 19:32:48 +0800

    save

--- app/src/main/java/cc/xiaojiang/liangbo/http/progress/ProgressObserver.java
+++ app/src/main/java/cc/xiaojiang/liangbo/http/progress/ProgressObserver.java
@@ -51,6 +51,9 @@ public abstract class ProgressObserver<T> implements Observer<T>, ProgressCancel
     @Override
     public void onError(Throwable e) {
         dismissProgressDialog();
+        if (e.getMessage() == null) {
+            ToastUtils.show("未知错误！");
+        }
         ToastUtils.show(e.getMessage());
         com.orhanobut.logger.Logger.e(e.getMessage());
         if (ApiException.class.isInstance(e)) {
